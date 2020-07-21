8d097ba3e93a080b78c17d092ccd9e90c9ef0365
Brandon Gart <brandongart@gmail.com>
2020-07-21 13:35:18 -0600

    adds router for workout

---
+++ routes/workout-api.js
@@ -0,0 +1,20 @@
+const db = require ("../models")
+const express = require("express")
+
+const router = express.Router();
+
+router.get("/api/workouts", async (req, res) => {
+
+});
+
+router.get("/api/workouts/range", async (req, res) => {
+
+});
+router.put("/api/workouts/:id", async (req, res) => {
+
+});
+router.post("/api/workouts", async (req, res) => {
+
+});
+
+module.exports = router;
