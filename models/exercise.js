d7f90176ad2c4d05e0459a54c929046b77199898
Brandon Gart <brandongart@gmail.com>
2020-07-24 15:02:23 -0600

    applied validation and defaults to models

--- models/exercise.js
+++ models/exercise.js
@@ -1,13 +1,22 @@
 const Schema = require("mongoose").Schema;
 
+function requiresWRS() {
+	return this.type === "resistance" && !this.duration;
+}
+
+function requiresDist() {
+	return this.type === "resistance" && !this.reps && !this.sets && this.weight;
+}
+
 // The permitted SchemaTypes are: String, Number, Date, Buffer, Boolean, Mixed, ObjectId, Array, Decimal128, Map
 const Exercise  = new Schema({
-	type: String,
-	name: String,
-	duration: Number,
-	weight: Number,
-	reps: Number,
-	sets: Number
+    type: {type: String, minlength: 1, maxlength: 16, required: "Exercises must have a type! "},
+    name: { type: String, minlength: 1, maxlength: 16, required: "Exercises must have a name! "},
+    duration: { type: Number, required: "Exercises must have a duration!", min:1 },
+    weight: { type: Number, required: requiresWRS, min:1 },
+    reps: { type:Number, required: requiresWRS, min:1 },
+	sets: { type:Number, required: requiresWRS, min:1 },
+	distance: { type: Number, required: requiresDist, min:1 }
     
 });
 
--- models/workout.js
+++ models/workout.js
@@ -2,7 +2,11 @@ const Schema = require("mongoose").Schema;
 
 // The permitted SchemaTypes are: String, Number, Date, Buffer, Boolean, Mixed, ObjectId, Array, Decimal128, Map
 const Workout  = new Schema({
-	day: Date,
+	//
+	day: {
+		type : Date,
+		default : () => new Date()
+	},
 	exercises: [{type: Schema.Types.ObjectId, ref: "exercise"}]
     
 });
