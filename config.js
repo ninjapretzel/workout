9e2d859f8f7dbc9b134d64e8e1f57a254451f1e9
ninjapretzel <ninjapretzel@yahoo.com>
2020-09-02 14:39:03 -0600

    Adds configuration for heroku

--- config.js
+++ config.js
@@ -1,6 +1,22 @@
-module.exports = {
-    port: 3000,
-    db: {
-        url: "localhost:27017/workoutTracker",
-    },
+const fs = require("fs");
+const DEV = fs.existsSync(".dev");
+
+const config = {
+	port: !DEV ? process.env.PORT : 3000,
+	db: {
+		url: "localhost:27017/workoutTracker",
+	},
 };
+
+if (!DEV) {
+	config.db = {
+		user: process.env.ATDB_USERNAME,
+		password: process.env.ATDB_PASSWORD,
+		host: process.env.ATDB_HOST,
+		dbname: process.env.ATDB_DBNAME, // Add ATDB_DBNAME to heroku console
+	}
+	const { user, password, host, dbname } = config.db;
+	config.db.url = `${user}:${password}@${host}/${dbname}?retryWrites=true&w=majority`
+}
+
+module.exports = config;
